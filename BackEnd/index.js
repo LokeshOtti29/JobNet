0969b308424520b545607184cc99aa903606fa95
Lokesh Otti <lokeshotti456@gmail.com>
2025-02-05 23:39:39 +0530

    Major Changes

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -8,8 +8,10 @@ const userRoute = require("./routes/userrouter");
 const companyroute = require("./routes/companyrouter");
 const jobroute = require("./routes/jobroute");
 const applicationroute = require("./routes/applicationroute");
+const path = require("path");
 dotenv.config({});
 
+const _dirname = path.resolve();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -28,6 +30,10 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyroute);
 app.use("/api/v1/job", jobroute);
 app.use("/api/v1/application", applicationroute);
+app.use(express.static(path.join(_dirname, "/fr-en/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "fr-en", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   connection();
